ca1b44f66197f432d60ffb2cebcf9e8fe7aabeaf
shige-99 <shige.99@outlook.jp>
2024-02-04 20:02:16 +0900

    💄 main部分の修正

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let nav = document.querySelector("#navArea");
 let btn = document.querySelector(".toggle-btn");
 let mask = document.querySelector("#mask");
+let navLinks = nav.querySelectorAll("a");
 
 btn.onclick = () => {
-  nav.classList.toggle("open");
-  let body = document.body;
-
-  // ナビゲーションバーの表示状態をトグル
-  nav.classList.toggle("is-active");
-
-  // bodyのスクロールを無効にするクラスをトグル
-  if (nav.classList.contains("is-active")) {
-    body.classList.add("no-scroll");
-  } else {
-    body.classList.remove("no-scroll");
-  }
+  toggleNav();
 };
 
 mask.onclick = () => {
-  nav.classList.toggle("open");
+  toggleNav();
 };
+
+// navArea の項目をクリックしたときに実行される関数
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleNav();
+  });
+});
+
+function toggleNav() {
+  nav.classList.toggle("open");
+  nav.classList.toggle("is-active");
+
+  // body のスクロールを無効/有効にする
+  document.body.classList.toggle(
+    "no-scroll",
+    nav.classList.contains("is-active")
+  );
+}
